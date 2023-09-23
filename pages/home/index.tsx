5dec8c4191ad0aee4a53a37ee2c8270758e2123d
MadHeo <tnlfl288@gmail.com>
2023-09-23 13:47:57 +0900

    First Setting

---
+++ pages/home/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <label>방문한 카페이름</label>
+        <input type="text" />
+        <label>닉네임</label>
+        <input type="text" />
+      </div>
+    </>
+  );
+}
