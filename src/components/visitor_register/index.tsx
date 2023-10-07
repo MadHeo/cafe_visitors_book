a036383bbd532043658a9f1adfcfb55fbe18e9c2
MadHeo <tnlfl288@gmail.com>
2023-10-07 16:48:54 +0900

    fix: 필터링 추가, 아이폰에서 입력 안되는 문제 수정

--- src/components/visitor_register/index.tsx
+++ src/components/visitor_register/index.tsx
@@ -18,6 +18,8 @@ import logo from "../../../public/img/logo_4.png";
 
 export default function VisitorRegister() {
   const router = useRouter();
+  const [contents, setContents] = useState();
+  const [name, setName] = useState();
 
   const [values, setValues] = useState({
     name: "",
@@ -28,10 +30,11 @@ export default function VisitorRegister() {
     const post = collection(getFirestore(firebaseApp), "post");
 
     void addDoc(post, {
-      name: values.name,
+      name: name,
       store: "마이오운가든",
-      create_at: format(new Date(), "yyy-MM-dd HH:mm:ss"),
-      content: values.content,
+      // create_at: format(new Date(), "yyy-MM-dd HH:mm:ss"),
+      create_at: new Date().toISOString().split("T")[0],
+      content: contents,
       yn: "y",
     });
 
@@ -43,6 +46,14 @@ export default function VisitorRegister() {
       ...values,
       [e.currentTarget.name]: e.currentTarget.value,
     });
+  };
+
+  const onChangeName = (e: any) => {
+    setName(e.currentTarget.value);
+  };
+
+  const onChangeContent = (e: any) => {
+    setContents(e.currentTarget.value);
   };
 
   return (
@@ -61,48 +72,48 @@ export default function VisitorRegister() {
           </Typography>
         </Box>
         <Box>
-          <form onSubmit={onClickSubmit}>
-            <Box marginBottom={"24px"}>
-              <InputLabel htmlFor="name">닉네임</InputLabel>
-              <Input
-                type="text"
-                id="name"
-                name="name"
-                onChange={handleChange}
-                sx={{ width: "100%" }}
-                inputProps={{ maxLength: 10 }}
-              />
-            </Box>
-            <Box marginBottom={"24px"}>
-              <InputLabel htmlFor="content">글 쓰기</InputLabel>
-              <TextField
-                id="content"
-                name="content"
-                onChange={handleChange}
-                multiline
-                rows={10}
-                inputProps={{ maxLength: 300 }}
-                placeholder="비하·비방·욕설이 담긴 게시물은 삭제될 수 있습니다. 언젠간 추억이 될 당신의 흔적을 남겨보세요. 최대 300자까지 입력할 수 있습니다."
-              />
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
+          {/* <form onSubmit={onClickSubmit}> */}
+          <Box marginBottom={"24px"}>
+            <InputLabel htmlFor="name">닉네임</InputLabel>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              onChange={onChangeName}
+              sx={{ width: "100%" }}
+              inputProps={{ maxLength: 10 }}
+            />
+          </Box>
+          <Box marginBottom={"24px"}>
+            <InputLabel htmlFor="content">글 쓰기</InputLabel>
+            <TextField
+              id="content"
+              name="content"
+              onChange={onChangeContent}
+              rows={10}
+              inputProps={{ maxLength: 300 }}
+              placeholder="비하·비방·욕설이 담긴 게시물은 삭제될 수 있습니다. 언젠간 추억이 될 당신의 흔적을 남겨보세요. 최대 300자까지 입력할 수 있습니다."
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              sx={{ width: "100%" }}
+              type="submit"
+              variant="contained"
+              size="large"
+              color="info"
+              onClick={onClickSubmit}
             >
-              <Button
-                sx={{ width: "100%" }}
-                type="submit"
-                variant="contained"
-                size="large"
-                color="info"
-              >
-                등록하기
-              </Button>
-            </Box>
-          </form>
+              등록하기
+            </Button>
+          </Box>
+          {/* </form> */}
           <Link href={"/myowngarden"}>
             <Button
               variant="outlined"
